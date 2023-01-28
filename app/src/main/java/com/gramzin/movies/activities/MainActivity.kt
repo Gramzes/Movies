ebead05499caa37cac8b79f66b6d699aa321671d
Gramzes <gramzin4666@gmail.com>
2023-01-28 14:49:31 +0300

    Initial commit

---
+++ app/src/main/java/com/gramzin/movies/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gramzin.movies.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gramzin.movies.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
